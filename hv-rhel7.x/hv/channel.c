370a55f8e83f3dda4c98697a2674efc0967a20b0
“Vivek <vyadav@microsoft.com>
2015-06-01 13:46:43 -0700

    Fix issue with RHEL7 boot , added vmbus_sendpacket function .

--- hv-rhel7.x/hv/channel.c
+++ hv-rhel7.x/hv/channel.c
@@ -569,20 +569,6 @@ void vmbus_close(struct vmbus_channel *channel)
 }
 EXPORT_SYMBOL_GPL(vmbus_close);
 
-/**
- * vmbus_sendpacket() - Send the specified buffer on the given channel
- * @channel: Pointer to vmbus_channel structure.
- * @buffer: Pointer to the buffer you want to receive the data into.
- * @bufferlen: Maximum size of what the the buffer will hold
- * @requestid: Identifier of the request
- * @type: Type of packet that is being send e.g. negotiate, time
- * packet etc.
- *
- * Sends data in @buffer directly to hyper-v via the vmbus
- * This will send the data unparsed to hyper-v.
- *
- * Mainly used by Hyper-V drivers.
- */
 int vmbus_sendpacket_ctl(struct vmbus_channel *channel, void *buffer,
 			   u32 bufferlen, u64 requestid,
 			   enum vmbus_packet_type type, u32 flags, bool kick_q)
@@ -632,6 +618,32 @@ int vmbus_sendpacket_ctl(struct vmbus_channel *channel, void *buffer,
 
 }
 EXPORT_SYMBOL(vmbus_sendpacket_ctl);
+
+/**
+ * vmbus_sendpacket() - Send the specified buffer on the given channel
+ * @channel: Pointer to vmbus_channel structure.
+ * @buffer: Pointer to the buffer you want to receive the data into.
+ * @bufferlen: Maximum size of what the the buffer will hold
+ * @requestid: Identifier of the request
+ * @type: Type of packet that is being send e.g. negotiate, time
+ * packet etc.
+ *
+ * Sends data in @buffer directly to hyper-v via the vmbus
+ * This will send the data unparsed to hyper-v.
+ *
+ * Mainly used by Hyper-V drivers.
+ */
+int vmbus_sendpacket(struct vmbus_channel *channel, void *buffer,
+                           u32 bufferlen, u64 requestid,
+                           enum vmbus_packet_type type, u32 flags)
+{
+        return vmbus_sendpacket_ctl(channel, buffer, bufferlen, requestid,
+                                    type, flags, true);
+}
+EXPORT_SYMBOL(vmbus_sendpacket);
+
+
+
 
 /*
  * vmbus_sendpacket_pagebuffer - Send a range of single-page buffer
