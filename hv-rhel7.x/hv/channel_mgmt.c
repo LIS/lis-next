a0631be6f00e878e70bf8f7080888484f54e4b0b
Simon XIAO <simonseattle@outlook.com>
2015-07-24 11:04:45 -0700

    Revert "Drivers: hv: vmbus: Handle both rescind and offer messages in the same context"
    This reverts commit 8514b1527cf1d3266ff90350cb25d28f1e5afb9a.

--- hv-rhel7.x/hv/channel_mgmt.c
+++ hv-rhel7.x/hv/channel_mgmt.c
@@ -134,36 +134,6 @@ fw_error:
 
 EXPORT_SYMBOL_GPL(vmbus_prep_negotiate_resp);
 
-
-static void vmbus_process_device_unregister(struct work_struct *work)
-{
-	struct device *dev;
-	struct vmbus_channel *channel = container_of(work,
-							struct vmbus_channel,
-							work);
-
-	dev = get_device(&channel->device_obj->device);
-	if (dev) {
-		vmbus_device_unregister(channel->device_obj);
-		put_device(dev);
-	}
-}
-
-static void vmbus_sc_creation_cb(struct work_struct *work)
-{
-	struct vmbus_channel *newchannel = container_of(work,
-							struct vmbus_channel,
-							work);
-	struct vmbus_channel *primary_channel = newchannel->primary_channel;
-
-	/*
-	 * On entry sc_creation_callback has been already verified to
-	 * be non-NULL.
-	 */
-	primary_channel->sc_creation_callback(newchannel);
-}
-
-
 /*
  * alloc_channel - Allocate and initialize a vmbus channel object
  */
@@ -234,6 +204,54 @@ static void percpu_channel_deq(void *arg)
 	list_del(&channel->percpu_list);
 }
 
+/*
+ * vmbus_process_rescind_offer -
+ * Rescind the offer by initiating a device removal
+ */
+static void vmbus_process_rescind_offer(struct work_struct *work)
+{
+	struct vmbus_channel *channel = container_of(work,
+						     struct vmbus_channel,
+						     work);
+	unsigned long flags;
+	struct vmbus_channel *primary_channel;
+	struct vmbus_channel_relid_released msg;
+	struct device *dev;
+
+	if (channel->device_obj) {
+		dev = get_device(&channel->device_obj->device);
+		if (dev) {
+			vmbus_device_unregister(channel->device_obj);
+			put_device(dev);
+		}
+	}
+
+	memset(&msg, 0, sizeof(struct vmbus_channel_relid_released));
+	msg.child_relid = channel->offermsg.child_relid;
+	msg.header.msgtype = CHANNELMSG_RELID_RELEASED;
+	vmbus_post_msg(&msg, sizeof(struct vmbus_channel_relid_released));
+
+	if (channel->target_cpu != get_cpu()) {
+		put_cpu();
+		smp_call_function_single(channel->target_cpu,
+					 percpu_channel_deq, channel, true);
+	} else {
+		percpu_channel_deq(channel);
+		put_cpu();
+	}
+
+	if (channel->primary_channel == NULL) {
+		spin_lock_irqsave(&vmbus_connection.channel_lock, flags);
+		list_del(&channel->listentry);
+		spin_unlock_irqrestore(&vmbus_connection.channel_lock, flags);
+	} else {
+		primary_channel = channel->primary_channel;
+		spin_lock_irqsave(&primary_channel->lock, flags);
+		list_del(&channel->sc_list);
+		spin_unlock_irqrestore(&primary_channel->lock, flags);
+	}
+	free_channel(channel);
+}
 
 void vmbus_free_channels(void)
 {
@@ -250,8 +268,11 @@ void vmbus_free_channels(void)
  * vmbus_process_offer - Process the offer by creating a channel/device
  * associated with this offer
  */
-static void vmbus_process_offer(struct vmbus_channel *newchannel)
+static void vmbus_process_offer(struct work_struct *work)
 {
+	struct vmbus_channel *newchannel = container_of(work,
+							struct vmbus_channel,
+							work);
 	struct vmbus_channel *channel;
 	bool fnew = true;
 	bool enq = false;
@@ -315,21 +336,9 @@ static void vmbus_process_offer(struct vmbus_channel *newchannel)
 
 			newchannel->state = CHANNEL_OPEN_STATE;
 			if (channel->sc_creation_callback != NULL)
-			
-				/*
-				 * We need to invoke the sub-channel creation
-				 * callback; invoke this in a seperate work
-				 * context since we are currently running on
-				 * the global work context in which we handle
-				 * messages from the host.
-				 */
-				INIT_WORK(&newchannel->work,
-					  vmbus_sc_creation_cb);
-				queue_work(newchannel->controlwq,
-					   &newchannel->work);
+				channel->sc_creation_callback(newchannel);
 
-			return;		
-
+			goto done_init_rescind;
 		}
 
 		goto err_free_chan;
@@ -370,7 +379,14 @@ static void vmbus_process_offer(struct vmbus_channel *newchannel)
 		kfree(newchannel->device_obj);
 		goto err_free_chan;
 	}
-
+done_init_rescind:
+	spin_lock_irqsave(&newchannel->lock, flags);
+	/* The next possible work is rescind handling */
+	INIT_WORK(&newchannel->work, vmbus_process_rescind_offer);
+	/* Check if rescind offer was already received */
+	if (newchannel->rescind)
+		queue_work(newchannel->controlwq, &newchannel->work);
+	spin_unlock_irqrestore(&newchannel->lock, flags);
 	return;
 err_free_chan:
 	free_channel(newchannel);
@@ -538,8 +554,9 @@ static void vmbus_onoffer(struct vmbus_channel_message_header *hdr)
 	       sizeof(struct vmbus_channel_offer_channel));
 	newchannel->monitor_grp = (u8)offer->monitorid / 32;
 	newchannel->monitor_bit = (u8)offer->monitorid % 32;
-	
-	vmbus_process_offer(newchannel);
+
+	INIT_WORK(&newchannel->work, vmbus_process_offer);
+	queue_work(newchannel->controlwq, &newchannel->work);
 }
 
 /*
@@ -555,30 +572,25 @@ static void vmbus_onoffer_rescind(struct vmbus_channel_message_header *hdr)
 
 	rescind = (struct vmbus_channel_rescind_offer *)hdr;
 	channel = relid2channel(rescind->child_relid);
-	
-	if (channel == NULL) {
-		hv_process_channel_removal(NULL, rescind->child_relid);
+
+	if (channel == NULL)
+		/* Just return here, no channel found */
 		return;
 
 	spin_lock_irqsave(&channel->lock, flags);
 	channel->rescind = true;
+	/*
+	 * channel->work.func != vmbus_process_rescind_offer means we are still
+	 * processing offer request and the rescind offer processing should be
+	 * postponed. It will be done at the very end of vmbus_process_offer()
+	 * as rescind flag is being checked there.
+	 */
+	if (channel->work.func == vmbus_process_rescind_offer)
+		/* work is initialized for vmbus_process_rescind_offer() from
+		 * vmbus_process_offer() where the channel got created */
+		queue_work(channel->controlwq, &channel->work);
 
 	spin_unlock_irqrestore(&channel->lock, flags);
-
-	if (channel->device_obj) {
-		/*
-		 * We will have to unregister this device from the
-		 * driver core. Do this in the per-channel work context.
-		 * Note that we are currently executing on the global
-		 * workq for handling messages from the host.
-		 */
-		INIT_WORK(&channel->work, vmbus_process_device_unregister);
-		queue_work(channel->controlwq, &channel->work);
-	} else {
-		hv_process_channel_removal(channel,
-					   channel->offermsg.child_relid);
-	}
-
 }
 
 /*
