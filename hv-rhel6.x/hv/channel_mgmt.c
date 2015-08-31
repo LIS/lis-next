cd22c42f78a7507d45718787b68355608db6c5a1
Vivek Yadav <vyadav@microsoft.com>
2015-08-31 16:10:54 -0700

    RH6 : Drivers: hv: vmbus: move init_vp_index() call to vmbus_process_offer()

--- hv-rhel6.x/hv/channel_mgmt.c
+++ hv-rhel6.x/hv/channel_mgmt.c
@@ -32,6 +32,9 @@
 
 #include "hyperv_vmbus.h"
 
+static void init_vp_index(struct vmbus_channel *channel,
+			  const uuid_le *type_guid);
+
 /**
  * vmbus_prep_negotiate_resp() - Create default response for Hyper-V Negotiate message
  * @icmsghdrp: Pointer to msg header structure
@@ -271,6 +274,8 @@ static void vmbus_process_offer(struct vmbus_channel *newchannel)
 		} else
 			goto err_free_chan;
 	}
+
+	init_vp_index(newchannel, &newchannel->offermsg.offer.if_type);
 
 	if (newchannel->target_cpu != get_cpu()) {
 		put_cpu();
@@ -514,8 +519,6 @@ static void vmbus_onoffer(struct vmbus_channel_message_header *hdr)
 		newchannel->sig_event->connectionid.u.id =
 				offer->connection_id;
 	}
-
-	init_vp_index(newchannel, &offer->offer.if_type);
 
 	memcpy(&newchannel->offermsg, offer,
 	       sizeof(struct vmbus_channel_offer_channel));
