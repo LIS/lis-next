3f1aeba8b3745ae0af9dc578c7ea6901e8b3cb4d
Nick Meier <nmeier@microsoft.com>
2016-01-14 13:35:44 -0800

    RH7: Inflight patch: scsi: storvsc: Install the storvsc specific timeout handler for FC devices

--- hv-rhel7.x/hv/storvsc_drv.c
+++ hv-rhel7.x/hv/storvsc_drv.c
@@ -48,6 +48,7 @@
 #include <scsi/scsi_devinfo.h>
 #include <scsi/scsi_dbg.h>
 #include <scsi/scsi_transport_fc.h>
+#include <scsi/scsi_transport.h>
 
 /*
  * All wire protocol details (storage protocol between the guest and the host)
@@ -2215,6 +2216,11 @@ static int __init storvsc_drv_init(void)
 	fc_transport_template = fc_attach_transport(&fc_transport_functions);
 	if (!fc_transport_template)
 		return -ENODEV;
+
+	/*
+	 * Install Hyper-V specific timeout handler.
+	 */
+	fc_transport_template->eh_timed_out = storvsc_eh_timed_out;
 #endif
 
 	ret = vmbus_driver_register(&storvsc_drv);
