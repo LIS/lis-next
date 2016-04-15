56629ddc006ec361a3b99fbfd1fd15610f7da4b8
Vivek Yadav <vyadav@microsoft.com>
2016-04-15 16:58:13 -0700

    RH7 : hv_netvsc: Eliminate is_data_pkt from struct hv_netvsc_packet

--- hv-rhel7.x/hv/hyperv_net.h
+++ hv-rhel7.x/hv/hyperv_net.h
@@ -129,7 +129,6 @@ struct hv_netvsc_packet {
 	/* Bookkeeping stuff */
 
 	u8 status;
-	u8 is_data_pkt;
 	u8 xmit_more; /* from skb */
 	u8 cp_partial; /* partial copy into send buffer */	
 	u8 rmsg_size; /* RNDIS header and PPI size */
