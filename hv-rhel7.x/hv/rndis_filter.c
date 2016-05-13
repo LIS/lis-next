a6bc00eefb63226ca51cdab11724bcc10a612cd2
Nick Meier <nmeier@microsoft.com>
2016-05-13 07:40:36 -0700

    RH7: hv_netvsc: Implement support for VF drivers on Hyper-V

--- hv-rhel7.x/hv/rndis_filter.c
+++ hv-rhel7.x/hv/rndis_filter.c
@@ -1232,6 +1232,9 @@ int rndis_filter_open(struct hv_device *dev)
 	if (!net_device)
 		return -EINVAL;
 
+	if (atomic_inc_return(&net_device->open_cnt) != 1)
+		return 0;
+
 	return rndis_filter_open_device(net_device->extension);
 }
 
@@ -1241,6 +1244,9 @@ int rndis_filter_close(struct hv_device *dev)
 
 	if (!nvdev)
 		return -EINVAL;
+
+	if (atomic_dec_return(&nvdev->open_cnt) != 0)
+		return 0;
 
 	return rndis_filter_close_device(nvdev->extension);
 }
