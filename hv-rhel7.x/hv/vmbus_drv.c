857a80d08c35670cd6219a35928bc85790e74d58
Vivek Yadav <vyadav@microsoft.com>
2016-09-17 15:33:32 -0700

    RH7: Drivers: hv: vmbus: add special crash handler

--- hv-rhel7.x/hv/vmbus_drv.c
+++ hv-rhel7.x/hv/vmbus_drv.c
@@ -1405,6 +1405,20 @@ static void hv_kexec_handler(void)
 };
 #endif
 
+#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,3))
+static void hv_crash_handler(struct pt_regs *regs)
+{
+	vmbus_initiate_unload();
+	/*
+	 * In crash handler we can't schedule synic cleanup for all CPUs,
+	 * doing the cleanup for current CPU only. This should be sufficient
+	 * for kdump.
+	 */
+	hv_synic_cleanup(NULL);
+	hv_cleanup();
+};
+#endif
+
 static int __init hv_acpi_init(void)
 {
 	int ret, t;
@@ -1442,6 +1456,7 @@ static int __init hv_acpi_init(void)
 		goto cleanup;
 #if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,3))
 	hv_setup_kexec_handler(hv_kexec_handler);
+	hv_setup_crash_handler(hv_crash_handler);
 #endif
 
 	return 0;
@@ -1455,8 +1470,9 @@ cleanup:
 static void __exit vmbus_exit(void)
 {
 	int cpu;
-#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,2))
+#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,3))
 	hv_remove_kexec_handler();
+	hv_remove_crash_handler();
 #endif
 	vmbus_connection.conn_state = DISCONNECTED;
 //	hv_synic_clockevents_cleanup();  will comment this for time being till clockevents_unbind showed up in distro code
