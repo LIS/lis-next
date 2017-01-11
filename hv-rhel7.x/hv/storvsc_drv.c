d1515443ff9b967712dd5c4d5be400a18613201d
Nick Meier <nmeier@microsoft.com>
2017-01-11 14:04:28 -0800

    RH7: scsi: storvsc: Enable tracking of queue depth

--- hv-rhel7.x/hv/storvsc_drv.c
+++ hv-rhel7.x/hv/storvsc_drv.c
@@ -2064,6 +2064,9 @@ static struct scsi_host_template scsi_driver = {
 	.use_clustering =	ENABLE_CLUSTERING,
 	/* Make sure we dont get a sg segment crosses a page boundary */
 	.dma_boundary =		PAGE_SIZE-1,
+#if (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7,3))
+	.track_queue_depth =	1,
+#endif
 };
 
 enum {
