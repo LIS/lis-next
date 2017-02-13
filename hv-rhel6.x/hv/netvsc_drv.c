3c69a80f4dd37a5766287443c7bc04d87d16ab05
Nick Meier <nmeier@microsoft.com>
2017-02-13 09:43:18 -0800

    RH6: netvsc: remove unused variables

--- hv-rhel6.x/hv/netvsc_drv.c
+++ hv-rhel6.x/hv/netvsc_drv.c
@@ -1515,7 +1515,6 @@ static int netvsc_vf_down(struct net_device *vf_netdev, bool rndis_close)
 static int netvsc_unregister_vf(struct net_device *vf_netdev)
 {
 	struct net_device *ndev;
-	struct netvsc_device *netvsc_dev;
 	struct net_device_context *net_device_ctx;
 
 	ndev = get_netvsc_byref(vf_netdev);
@@ -1523,7 +1522,6 @@ static int netvsc_unregister_vf(struct net_device *vf_netdev)
 		return NOTIFY_DONE;
 
 	net_device_ctx = netdev_priv(ndev);
-	netvsc_dev = net_device_ctx->nvdev;
 
 	netvsc_vf_down(vf_netdev, false);
 
@@ -1645,7 +1643,6 @@ static int netvsc_remove(struct hv_device *dev)
 {
 	struct net_device *net;
 	struct net_device_context *ndev_ctx;
-	struct netvsc_device *net_device;
 
 	net = hv_get_drvdata(dev);
 
@@ -1655,7 +1652,6 @@ static int netvsc_remove(struct hv_device *dev)
 	}
 
 	ndev_ctx = netdev_priv(net);
-	net_device = ndev_ctx->nvdev;
 
 	/* Avoid racing with netvsc_change_mtu()/netvsc_set_channels()
 	 * removing the device.
