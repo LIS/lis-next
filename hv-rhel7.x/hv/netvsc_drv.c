ab485b54b2f6d2f5976a25b0a483057844d91964
simonxiao <sixiao@microsoft.com>
2017-02-21 16:02:18 -0800

    [hv-rhel7.x] netvsc: simplify rndis_filter_remove
    author: stephen hemminger <stephen@networkplumber.org>
    commit: 2289f0aa706e5160e078f73c32fcbfb56a3ff1e2
    
    All caller's already have pointer to netvsc_device so pass it.

--- hv-rhel7.x/hv/netvsc_drv.c
+++ hv-rhel7.x/hv/netvsc_drv.c
@@ -888,7 +888,7 @@ static int netvsc_set_channels(struct net_device *net,
 		return ret;
 
 	net_device_ctx->start_remove = true;
-	rndis_filter_device_remove(dev);
+	rndis_filter_device_remove(dev, nvdev);
 
 	ret = netvsc_set_queues(net, dev, count);
 	if (ret == 0)
@@ -980,7 +980,7 @@ static int netvsc_change_mtu(struct net_device *ndev, int mtu)
 		goto out;
 
 	ndevctx->start_remove = true;
-	rndis_filter_device_remove(hdev);
+	rndis_filter_device_remove(hdev, nvdev);
 
 	ndev->mtu = mtu;
 
@@ -1620,7 +1620,7 @@ static int netvsc_probe(struct hv_device *dev,
 	ret = register_netdev(net);
 	if (ret != 0) {
 		pr_err("Unable to register netdev.\n");
-		rndis_filter_device_remove(dev);
+		rndis_filter_device_remove(dev, nvdev);
 		netvsc_free_netdev(net);
 	}
 
@@ -1660,7 +1660,7 @@ static int netvsc_remove(struct hv_device *dev)
 	 * Call to the vsc driver to let it know that the device is being
 	 * removed
 	 */
-	rndis_filter_device_remove(dev);
+	rndis_filter_device_remove(dev, ndev_ctx->nvdev);
 
 	hv_set_drvdata(dev, NULL);
 
