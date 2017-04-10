6125a02eb298881b48ebf7d3b5ffc47f64871205
Haiyang Zhang <haiyangz@microsoft.com>
2017-04-10 14:26:30 -0700

    RH6: netvsc: handle select_queue when device is being removed
    Move the send indirection table from the inner device (netvsc)
    to the network device context.
    
    It is possible that netvsc_device is not present (remove in progress).
    This solves potential use after free issues when packet is being
    created during MTU change, shutdown, or queue count changes.
    
    Fixes: d8e18ee0fa96 ("netvsc: enhance transmit select_queue")
    Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Upstream: 7ce101246655935b014b11d81f815342921f5654

--- hv-rhel6.x/hv/netvsc_drv.c
+++ hv-rhel6.x/hv/netvsc_drv.c
@@ -203,19 +203,15 @@ static u16 netvsc_select_queue(struct net_device *ndev, struct sk_buff *skb,
 static u16 netvsc_select_queue(struct net_device *ndev, struct sk_buff *skb)
 {
 	struct net_device_context *net_device_ctx = netdev_priv(ndev);
-	struct netvsc_device *nvsc_dev = net_device_ctx->nvdev;
 	u32 hash;
 	u16 q_idx = 0;
 
-	if (nvsc_dev == NULL || ndev->real_num_tx_queues <= 1)
+	if (ndev->real_num_tx_queues <= 1)
 		return 0;
 
 	hash = skb_get_hash(skb);
-	q_idx = nvsc_dev->send_table[hash % VRSS_SEND_TAB_SIZE] %
+	q_idx = net_device_ctx->tx_send_table[hash % VRSS_SEND_TAB_SIZE] %
 		ndev->real_num_tx_queues;
-
-	if (unlikely(!nvsc_dev->chan_table[q_idx].channel))
-		q_idx = 0;
 
 	return q_idx;
 }
