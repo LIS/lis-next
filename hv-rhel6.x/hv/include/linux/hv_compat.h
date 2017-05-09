89e13c8281311cfe90061f67b0ab504751481053
Chris Valean <v-chvale@microsoft.com>
2017-05-09 12:00:56 -0400

    RH6: functions emulation

--- hv-rhel6.x/hv/include/linux/hv_compat.h
+++ hv-rhel6.x/hv/include/linux/hv_compat.h
@@ -675,6 +675,14 @@ static inline u32 ethtool_rxfh_indir_default(u32 index, u32 n_rx_rings)
 }
 #endif
 
+#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(6,4))
+#define rtnl_dereference(ptr) (ptr)
+
+#ifdef rcu_dereference_rtnl
+#define rcu_dereference_rtnl(p)  rcu_dereference(p)
+#endif
+#endif
+
 #ifndef NAPI_POLL_WEIGHT
 #define NAPI_POLL_WEIGHT 64
 #endif
