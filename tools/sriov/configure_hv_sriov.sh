759662116349c0bfff0e2be3d4a7320fe1959f9e
Simon Xiao <sixiao@microsoft.com>
2017-06-23 00:31:20 -0700

    [SRIOV] sriov helper script: change the way to configure with one reboot only
    The previous implementation is:
    -- Download udev rule file and it will trigger bondvf.sh,
    when vf device shows up;
    -- But with this way, user may need to reboot system twice:
    The first reboot is for vf show up, and trigger the vf name change and
    call bondvf.sh;
    The second reboot is for bond driver loads the bond config files.
    
    Now, change the way to:
    -- After VM provisioned, user call this helper script;
    -- This script will write bond0/vf1/eth0 config to the right place;
    -- Reboot, then bonding driver will load all the NIC config.

--- tools/sriov/configure_hv_sriov.sh
+++ tools/sriov/configure_hv_sriov.sh
@@ -23,25 +23,16 @@
 
 ######################################################################## 
 #
-# This is a script to configure SRIOV for Ubuntu 1604 VM on Azure.
+# This is a script to configure SRIOV for Linux VMs on Azure.
 # 
 # How to use this:
-#    $sudo ./configure_sriov_ubuntu1604.sh
+#    $sudo ./configure_hv_sriov.sh
 # Logging:
-#    See log: "/var/log/configure_sriov_ubuntu1604.log"
+#    See log: "/var/log/configure_hv_sriov.log"
 #
 ########################################################################
 
-log_file="/var/log/configure_sriov_ubuntu1604.log"
-bin_folder="/usr/sbin/"
-udev_folder="/etc/udev/rules.d/"
-udev_file="60-hyperv-sriov.rules"
-hv_vf_name_file="hv_vf_name"
-bondvf_lock_file="bondvf_lock"
-bondvf_sh_file="bondvf.sh"
-eth0_dhcp_config_line1="auto eth0"
-eth0_dhcp_config_line2="iface eth0 inet dhcp"
-all_files_downloaded=true
+log_file="/var/log/configure_hv_sriov.log"
 
 ########################################
 # Detect Distro
@@ -72,45 +63,95 @@ LOG "---------------------------------------"
 LOG "Configure SRIOV for $distro"
 
 ########################################
-# Download files
-########################################
-LOG "Start downloading configuration files ..."
-cd /tmp
-wget "https://raw.githubusercontent.com/LIS/lis-next/master/tools/sriov/60-hyperv-sriov.rules"
-wget "https://raw.githubusercontent.com/LIS/lis-next/master/tools/sriov/hv_vf_name"
-wget "https://raw.githubusercontent.com/LIS/lis-next/master/tools/sriov/bondvf_lock"
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/plain/tools/hv/bondvf.sh"
-
-LOG "Move configuration to the destination folder ..."
-mv -f $udev_file $udev_folder
-chmod +x $hv_vf_name_file $bondvf_lock_file $bondvf_sh_file
-mv -f $hv_vf_name_file  $bin_folder
-mv -f $bondvf_lock_file $bin_folder
-mv -f $bondvf_sh_file   $bin_folder
-
-########################################
-# Ubuntu 1604 only:
-# Change eth0 DHCP configuration
+# Write configuration for:
+# vf, synthetic NIC, bond interfaces 
 ########################################
 if [ $distro == 'ubuntu' ]; then
     default_net_config=$cfgdir/interfaces
-    LOG "Running on Ubuntu: making change to the eth0 DHCP configuration ..."
+
+    LOG "Running on Ubuntu: making change to ${default_net_config} ..."
     sed -i 's/^source/#source/' $default_net_config
-    echo "$eth0_dhcp_config_line1" >> $default_net_config
-    echo "$eth0_dhcp_config_line2" >> $default_net_config
+    echo "allow-hotplug vf1"       >> $default_net_config
+    echo "iface vf1 inet manual"   >> $default_net_config
+    echo "bond-master bond0"       >> $default_net_config
+    echo "bond-primary vf1"        >> $default_net_config
+    echo ""                        >> $default_net_config
+    echo "auto eth0"               >> $default_net_config
+    echo "iface eth0 inet manual"  >> $default_net_config
+    echo "bond-master bond0"       >> $default_net_config
+    echo ""                        >> $default_net_config
+    echo "auto bond0"              >> $default_net_config
+    echo "iface bond0 inet dhcp"   >> $default_net_config
+    echo "bond-mode active-backup" >> $default_net_config
+    echo "bond-miimon 100"         >> $default_net_config
+    echo "bond-slaves none"        >> $default_net_config
 
-    n_line1=`cat $default_net_config | grep "$eth0_dhcp_config_line1" | wc -l`
+    n_line1=`cat $default_net_config | grep "allow-hotplug" | grep "vf1" | wc -l`
     if [ $n_line1 -ne 1 ]; then
-        LOG "[Failed] $n_line1 line(s) of '${eth0_dhcp_config_line1}' in $default_net_config"
+        LOG "[Failed] allow-hotplug vf1: was defined multiple times in ${default_net_config}"
         exit
     fi
 
-    n_line2=`cat $default_net_config | grep "$eth0_dhcp_config_line2" | wc -l`
+    n_line2=`cat $default_net_config | grep "auto" | grep "eth0" | wc -l`
     if [ $n_line2 -ne 1 ]; then
-        LOG "[Failed] $n_line2 line(s) of '${eth0_dhcp_config_line2}' in $default_net_config"
+        LOG "[Failed] auto eth0: was defined multiple times in ${default_net_config}"
         exit
     fi
+	
+	n_line3=`cat $default_net_config | grep "auto" | grep "bond0" | wc -l`
+    if [ $n_line3 -ne 1 ]; then
+        LOG "[Failed] auto bond0: was defined multiple times in ${default_net_config}"
+        exit
+    fi
+elif [ $distro == 'suse' ]; then
+    ifcfg_bond0_cfg=$cfgdir/ifcfg-bond0
+    ifcfg_eth0_cfg=$cfgdir/ifcfg-eth0
+    ifcfg_eth0_old_cfg=$cfgdir/old.ifcfg-eth0.backup
+    ifcfg_vf1_cfg=$cfgdir/ifcfg-vf1
+
+    LOG "Running on Suse: making change to:"
+    LOG "${ifcfg_bond0_cfg} ..."
+	echo "BOOTPROTO=dhcp"       >  $ifcfg_bond0_cfg
+	echo "STARTMODE=auto"       >> $ifcfg_bond0_cfg
+	echo "BONDING_MASTER=yes"   >> $ifcfg_bond0_cfg
+	echo "BONDING_SLAVE_0=vf1"  >> $ifcfg_bond0_cfg
+	echo "BONDING_SLAVE_1=eth0" >> $ifcfg_bond0_cfg
+	echo "BONDING_MODULE_OPTS='mode=active-backup miimon=100 primary=vf1'"  >> $ifcfg_bond0_cfg
+
+    LOG "${ifcfg_eth0_cfg} ..."
+	mv $ifcfg_eth0_cfg $ifcfg_eth0_old_cfg
+	echo "BOOTPROTO=none"       >  $ifcfg_eth0_cfg
+	echo "STARTMODE=auto"       >> $ifcfg_eth0_cfg
+	
+    LOG "${ifcfg_vf1_cfg} ..."
+	echo "BOOTPROTO=none"       >  $ifcfg_vf1_cfg
+	echo "STARTMODE=hotplug"    >> $ifcfg_vf1_cfg
+else
+    echo "Unsupported distro. Exiting."
+    exit 1
 fi
+
+########################################
+# Download files
+########################################
+bin_folder="/usr/sbin/"
+udev_folder="/etc/udev/rules.d/"
+udev_file="60-hyperv-vf-name.rules"
+hv_vf_name_file="hv_vf_name"
+bondvf_sh_file="bondvf.sh"
+all_files_downloaded=true
+
+LOG "Start downloading udev rule and config scripts ..."
+cd /tmp
+wget "https://raw.githubusercontent.com/LIS/lis-next/master/tools/sriov/${udev_file}"
+wget "https://raw.githubusercontent.com/LIS/lis-next/master/tools/sriov/${hv_vf_name_file}"
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/plain/tools/hv/${bondvf_sh_file}"
+
+LOG "Move configuration to the destination folder ..."
+mv -f $udev_file $udev_folder
+chmod +x $hv_vf_name_file $bondvf_sh_file
+mv -f $hv_vf_name_file  $bin_folder
+mv -f $bondvf_sh_file   $bin_folder
 
 ########################################
 # Check downloaded files
@@ -124,11 +165,6 @@ fi
 if [ ! -f ${bin_folder}${hv_vf_name_file} ]; then
     all_files_downloaded=false
     LOG "${hv_vf_name_file} is not found in ${bin_folder}!"
-fi
-
-if [ ! -f ${bin_folder}${bondvf_lock_file} ]; then
-    all_files_downloaded=false
-    LOG "${bondvf_lock_file} is not found in ${bin_folder}!"
 fi
 
 if [ ! -f ${bin_folder}${bondvf_sh_file} ]; then
