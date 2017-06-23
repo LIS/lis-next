0934efc75f2accb120efde5b6be3ab4ecd591138
Chris Valean <cvalean@cloudbasesolutions.com>
2017-06-23 12:50:31 +0300

    53fa1a6f33520f01f9dbee48369074b34d77616b
    RH7: hv_netvsc: Fix the carrier state error when data path is off

--- hv-rhel7.x/hv/hyperv_net.h
+++ hv-rhel7.x/hv/hyperv_net.h
@@ -719,6 +719,9 @@ struct net_device_context {
 	u32 vf_alloc;
 	/* Serial number of the VF to team with */
 	u32 vf_serial;
+
+	bool datapath;	/* 0 - synthetic, 1 - VF nic */
+
 	/*
 	 * Tracks the current data path; initially the data path is set
 	 * to deliver packets on the synthetic path.
