2e7c6f48a3ee1ca5e2e9eb30183d54b46388a62e
Chris Valean <cvalean@cloudbasesolutions.com>
2017-07-24 11:08:46 +0300

    RH7: netvsc: add some rtnl_dereference annotations
    79e8cbe7a789a0863cc6cd874872b4dd63ec5947
    
    In a couple places RTNL is held, and the netvsc_device pointer
    is acquired without annotation.

--- hv-rhel7.x/hv/netvsc_drv.c
+++ hv-rhel7.x/hv/netvsc_drv.c
@@ -75,7 +75,7 @@ static int netvsc_open(struct net_device *net)
 {
 	struct net_device_context *ndev_ctx = netdev_priv(net);
 	struct net_device *vf_netdev = rtnl_dereference(ndev_ctx->vf_netdev);
-	struct netvsc_device *nvdev = ndev_ctx->nvdev;
+	struct netvsc_device *nvdev = rtnl_dereference(ndev_ctx->nvdev);
 	struct rndis_device *rdev;
 	int ret;
 
@@ -1560,7 +1560,7 @@ static struct net_device *get_netvsc_byref(struct net_device *vf_netdev)
 			continue;	/* not a netvsc device */
 
 		net_device_ctx = netdev_priv(dev);
-		if (net_device_ctx->nvdev == NULL)
+		if (!rtnl_dereference(net_device_ctx->nvdev))
 			continue;	/* device is removed */
 
 		if (rtnl_dereference(net_device_ctx->vf_netdev) == vf_netdev)
@@ -1919,7 +1919,8 @@ static int netvsc_remove(struct hv_device *dev)
 	 * removed. Also blocks mtu and channel changes.
 	 */
 	rtnl_lock();
-	rndis_filter_device_remove(dev, ndev_ctx->nvdev);
+	rndis_filter_device_remove(dev,
+				   rtnl_dereference(ndev_ctx->nvdev));
 	rtnl_unlock();
 
 	unregister_netdev(net);
