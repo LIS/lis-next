7b5eca697f87c31c1d7e5f27508057f110a55e3e
Alex Ng <alexng@microsoft.com>
2017-07-25 17:45:09 -0700

    RH6: netvsc: use refcount_t for keeping track of sub channels
    Upstream: 43c7bd1ffcd1621c64cedf1be52156e2f95bba9b
    
    Rather than a lock and variable, use a refcount_t to keep track
    of the number of sub channels.  Don't need to wait for subchannels
    on device removal since wait was already done in device_add.
    
    Also fix the error handling; don't wait forever in case of
    an error on request to create sub channels.
    
    Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- hv-rhel6.x/hv/hyperv_net.h
+++ hv-rhel6.x/hv/hyperv_net.h
@@ -784,8 +784,8 @@ struct netvsc_device {
 
 	u32 max_chn;
 	u32 num_chn;
-	spinlock_t sc_lock; /* Protects num_sc_offered variable */
-	u32 num_sc_offered;
+
+	refcount_t sc_offered;
 
 	struct rndis_device *extension;
 
