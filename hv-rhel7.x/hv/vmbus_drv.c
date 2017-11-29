LIS/lis-next

commit 8e4be427dfcbe811c27fbde4040c1f90de5b59ea

Modifying RHEL7 code to use lis clocksource, remove mshyperv.c and hv_timsource.c

hv-rhel7.x/hv/vmbus_drv.c
@@ -45,6 +45,32 @@ static struct tasklet_struct msg_dpc;
 static struct completion probe_event;
 static int irq;
 
+
+int hyperv_panic_event(struct notifier_block *nb,
+                        unsigned long event, void *ptr)
+{
+        struct pt_regs *regs;
+
+        regs = task_pt_regs(current);
+
+        wrmsrl(HV_X64_MSR_CRASH_P0, regs->ip);
+        wrmsrl(HV_X64_MSR_CRASH_P1, regs->ax);
+        wrmsrl(HV_X64_MSR_CRASH_P2, regs->bx);
+        wrmsrl(HV_X64_MSR_CRASH_P3, regs->cx);
+        wrmsrl(HV_X64_MSR_CRASH_P4, regs->dx);
+
+        /*
+         * Let Hyper-V know there is crash data available
+         */
+        wrmsrl(HV_X64_MSR_CRASH_CTL, HV_CRASH_CTL_CRASH_NOTIFY);
+        return NOTIFY_DONE;
+}
+
+static struct notifier_block hyperv_panic_block = {
+        .notifier_call = hyperv_panic_event,
+};
+
+
 struct resource hyperv_mmio = {
 	.name  = "hyperv mmio",
 	.flags = IORESOURCE_MEM,
@@ -737,6 +763,36 @@ static void vmbus_flow_handler(unsigned int irq, struct irq_desc *desc)
 	desc->action->handler(irq, desc->action->dev_id);
 }
 
+#define HV_CLOCK_SHIFT  22
+
+static cycle_t read_hv_clock(struct clocksource *arg)
+{
+        cycle_t current_tick;
+        /*
+         * Read the partition counter to get the current tick count. This count
+         * is set to 0 when the partition is created and is incremented in
+         * 100 nanosecond units.
+         */
+        rdmsrl(HV_X64_MSR_TIME_REF_COUNT, current_tick);
+        return current_tick;
+}
+
+static struct clocksource hyperv_cs = {
+        .name           = "hyperv_clocksource_lis",
+        .rating         = 499, /* use this when running on Hyperv*/
+        .read           = read_hv_clock,
+        .mask           = CLOCKSOURCE_MASK(64),
+        /*
+         * The time ref counter in HyperV is in 100ns units.
+         * The definition of mult is:
+         * mult/2^shift = ns/cyc = 100
+         * mult = (100 << shift)
+         */
+        .mult           = (100 << HV_CLOCK_SHIFT),
+        .shift          = HV_CLOCK_SHIFT,
+        .flags          = CLOCK_SOURCE_VALID_FOR_HRES,
+};
+
 /*
  * vmbus_bus_init -Main vmbus driver initialization routine.
  *
@@ -799,8 +855,17 @@ static int vmbus_bus_init(int irq)
 	if (ret)
 		goto err_alloc;
 
-	vmbus_request_offers();
-
+	/*
+         * Only register if the crash MSRs are available
+         */
+        if (ms_hyperv.features & HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE) {
+                atomic_notifier_chain_register(&panic_notifier_list,
+                                               &hyperv_panic_block);
+        }
+        vmbus_request_offers();
+        if (ms_hyperv.features & HV_X64_MSR_TIME_REF_COUNT_AVAILABLE)
+                clocksource_register(&hyperv_cs);
+	
 	return 0;
 
 err_alloc:

hv-rhel7.x/hv/hv_timesource.c
@@ -1,100 +1,0 @@
-/*
- * A clocksource for Linux running on HyperV.
- *
- *
- * Copyright (C) 2010, Novell, Inc.
- * Author : K. Y. Srinivasan <ksrinivasan@novell.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
- * NON INFRINGEMENT.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-#include <linux/clocksource.h>
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/pci.h>
-#include <linux/dmi.h>
-#include <asm/hyperv.h>
-#include <asm/mshyperv.h>
-
-#define HV_CLOCK_SHIFT	22
-
-static cycle_t read_hv_clock(void)
-{
-	cycle_t current_tick;
-	/*
-	 * Read the partition counter to get the current tick count. This count
-	 * is set to 0 when the partition is created and is incremented in
-	 * 100 nanosecond units.
-	 */
-	rdmsrl(HV_X64_MSR_TIME_REF_COUNT, current_tick);
-	return current_tick;
-}
-
-static struct clocksource hyperv_cs = {
-	.name           = "hyperv_clocksource",
-	.rating         = 499, /* use this when running on Hyperv*/
-	.read           = read_hv_clock,
-	.mask           = CLOCKSOURCE_MASK(64),
-	/*
-	 * The time ref counter in HyperV is in 100ns units.
-	 * The definition of mult is:
-	 * mult/2^shift = ns/cyc = 100
-	 * mult = (100 << shift)
-	 */
-	.mult           = (100 << HV_CLOCK_SHIFT),
-	.shift          = HV_CLOCK_SHIFT,
-};
-
-static struct dmi_system_id
-hv_timesource_dmi_table[]  = {
-	{
-		.ident = "Hyper-V",
-		.matches = {
-			DMI_MATCH(DMI_SYS_VENDOR, "Microsoft Corporation"),
-			DMI_MATCH(DMI_PRODUCT_NAME, "Virtual Machine"),
-			DMI_MATCH(DMI_BOARD_NAME, "Virtual Machine"),
-		},
-	},
-	{ },
-};
-MODULE_DEVICE_TABLE(dmi, hv_timesource_dmi_table);
-
-static const struct pci_device_id
-hv_timesource_pci_table[]  = {
-	{ PCI_DEVICE(0x1414, 0x5353) }, /* VGA compatible controller */
-	{ 0 }
-};
-MODULE_DEVICE_TABLE(pci, hv_timesource_pci_table);
-
-
-static int __init init_hv_clocksource(void)
-{
-	if ((x86_hyper != &x86_hyper_ms_hyperv) ||
-		!(ms_hyperv.features & HV_X64_MSR_TIME_REF_COUNT_AVAILABLE))
-		return -ENODEV;
-
-	if (!dmi_check_system(hv_timesource_dmi_table))
-		return -ENODEV;
-
-	pr_info("Registering HyperV clock source\n");
-	return  clocksource_register(&hyperv_cs);
-}
-
-module_init(init_hv_clocksource);
-MODULE_DESCRIPTION("HyperV based clocksource");
-MODULE_AUTHOR("K. Y. Srinivasan <ksrinivasan@novell.com>");
-MODULE_LICENSE("GPL");

hv-rhel7.x/hv/hyperv_vmbus.h
@@ -49,6 +49,17 @@ enum hv_cpuid_function {
 	HVCPUID_IMPLEMENTATION_LIMITS		= 0x40000005,
 };
 
+#define  HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE   0x400
+
+#define HV_X64_MSR_CRASH_P0   0x40000100
+#define HV_X64_MSR_CRASH_P1   0x40000101
+#define HV_X64_MSR_CRASH_P2   0x40000102
+#define HV_X64_MSR_CRASH_P3   0x40000103
+#define HV_X64_MSR_CRASH_P4   0x40000104
+#define HV_X64_MSR_CRASH_CTL  0x40000105
+
+#define HV_CRASH_CTL_CRASH_NOTIFY (1ULL << 63)
+
 /* Define version of the synthetic interrupt controller. */
 #define HV_SYNIC_VERSION		(1)
 

hv-rhel7.x/hv/mshyperv.c
@@ -1,153 +1,0 @@
-/*
- * HyperV  Detection code.
- *
- * Copyright (C) 2010, Novell, Inc.
- * Author : K. Y. Srinivasan <ksrinivasan@novell.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
- *
- */
-
-#include <linux/types.h>
-#include <linux/time.h>
-#include <linux/clocksource.h>
-#include <linux/module.h>
-#include <linux/hardirq.h>
-#include <linux/efi.h>
-#include <linux/interrupt.h>
-#include <linux/irq.h>
-#include <asm/processor.h>
-#include <asm/hypervisor.h>
-#include <asm/hyperv.h>
-#include <asm/mshyperv.h>
-#include <asm/desc.h>
-#include <asm/idle.h>
-#include <asm/irq_regs.h>
-#include <asm/i8259.h>
-#include <asm/apic.h>
-#include <asm/timer.h>
-
-struct ms_hyperv_info ms_hyperv;
-EXPORT_SYMBOL_GPL(ms_hyperv);
-
-#if IS_ENABLED(CONFIG_HYPERV)
-static void (*vmbus_handler)(void);
-
-void hyperv_vector_handler(struct pt_regs *regs)
-{
-	struct pt_regs *old_regs = set_irq_regs(regs);
-
-	irq_enter();
-	exit_idle();
-
-	inc_irq_stat(irq_hv_callback_count);
-	if (vmbus_handler)
-		vmbus_handler();
-
-	irq_exit();
-	set_irq_regs(old_regs);
-}
-
-void hv_setup_vmbus_irq(void (*handler)(void))
-{
-	vmbus_handler = handler;
-	/*
-	 * Setup the IDT for hypervisor callback. Prevent reallocation
-	 * at module reload.
-	 */
-	if (!test_bit(HYPERVISOR_CALLBACK_VECTOR, used_vectors))
-		alloc_intr_gate(HYPERVISOR_CALLBACK_VECTOR,
-				hyperv_callback_vector);
-}
-
-void hv_remove_vmbus_irq(void)
-{
-	/* We have no way to deallocate the interrupt gate */
-	vmbus_handler = NULL;
-}
-EXPORT_SYMBOL_GPL(hv_setup_vmbus_irq);
-EXPORT_SYMBOL_GPL(hv_remove_vmbus_irq);
-#endif
-
-static uint32_t  __init ms_hyperv_platform(void)
-{
-	u32 eax;
-	u32 hyp_signature[3];
-
-	if (!boot_cpu_has(X86_FEATURE_HYPERVISOR))
-		return 0;
-
-	cpuid(HYPERV_CPUID_VENDOR_AND_MAX_FUNCTIONS,
-	      &eax, &hyp_signature[0], &hyp_signature[1], &hyp_signature[2]);
-
-	if (eax >= HYPERV_CPUID_MIN &&
-	    eax <= HYPERV_CPUID_MAX &&
-	    !memcmp("Microsoft Hv", hyp_signature, 12))
-		return HYPERV_CPUID_VENDOR_AND_MAX_FUNCTIONS;
-
-	return 0;
-}
-
-static cycle_t read_hv_clock(struct clocksource *arg)
-{
-	cycle_t current_tick;
-	/*
-	 * Read the partition counter to get the current tick count. This count
-	 * is set to 0 when the partition is created and is incremented in
-	 * 100 nanosecond units.
-	 */
-	rdmsrl(HV_X64_MSR_TIME_REF_COUNT, current_tick);
-	return current_tick;
-}
-
-static struct clocksource hyperv_cs = {
-	.name		= "hyperv_clocksource",
-	.rating		= 400, /* use this when running on Hyperv*/
-	.read		= read_hv_clock,
-	.mask		= CLOCKSOURCE_MASK(64),
-	.flags		= CLOCK_SOURCE_IS_CONTINUOUS,
-};
-
-static void __init ms_hyperv_init_platform(void)
-{
-	/*
-	 * Extract the features and hints
-	 */
-	ms_hyperv.features = cpuid_eax(HYPERV_CPUID_FEATURES);
-	ms_hyperv.hints    = cpuid_eax(HYPERV_CPUID_ENLIGHTMENT_INFO);
-
-	printk(KERN_INFO "HyperV: features 0x%x, hints 0x%x\n",
-	       ms_hyperv.features, ms_hyperv.hints);
-
-#ifdef CONFIG_X86_LOCAL_APIC
-	if (ms_hyperv.features & HV_X64_MSR_APIC_FREQUENCY_AVAILABLE) {
-		/*
-		 * Get the APIC frequency.
-		 */
-		u64	hv_lapic_frequency;
-
-		rdmsrl(HV_X64_MSR_APIC_FREQUENCY, hv_lapic_frequency);
-		hv_lapic_frequency = div_u64(hv_lapic_frequency, HZ);
-		lapic_timer_frequency = hv_lapic_frequency;
-		printk(KERN_INFO "HyperV: LAPIC Timer Frequency: %#x\n",
-				lapic_timer_frequency);
-	}
-#endif
-
-	if (ms_hyperv.features & HV_X64_MSR_TIME_REF_COUNT_AVAILABLE)
-		clocksource_register_hz(&hyperv_cs, NSEC_PER_SEC/100);
-
-#ifdef CONFIG_X86_IO_APIC
-	no_timer_check = 1;
-#endif
-
-}
-
-const __refconst struct hypervisor_x86 x86_hyper_ms_hyperv = {
-	.name			= "Microsoft HyperV",
-	.detect			= ms_hyperv_platform,
-	.init_platform		= ms_hyperv_init_platform,
-};
-EXPORT_SYMBOL(x86_hyper_ms_hyperv);

hv-rhel7.x/hv/rhel5-hv-driver-install
@@ -1,35 +1,0 @@
-echo "Building Modules"
-
-make -C /lib/modules/$(uname -r)/build M=`pwd` clean
-make -C /lib/modules/$(uname -r)/build M=`pwd` modules
-
-echo "Installing Modules"
-
-cp -f ./*.ko /lib/modules/$(uname -r)/extra/
-
-echo "Generating Module dependencies"
-
-depmod
-
-echo "Installing Modprobe Rules"
-
-
-echo "Saving old initramfs"
-
-if [ ! -e /boot/"initrd-$(uname -r).img-old" ]
-then
-	cp -f /boot/"initrd-$(uname -r).img" /boot/"initrd-$(uname -r).img-old"
-fi
-
-echo "Generating initramfs"
-
-mkinitrd --preload=hv_storvsc "initrd-$(uname -r).img" "$(uname -r)"
-
-echo "Installing new initramfs"
-
-mv -f "initrd-$(uname -r).img" /boot/"initrd-$(uname -r).img"
-
-echo "Config X-Window"
-
-cp -f ./xorg.conf /etc/X11/
-

hv-rhel7.x/hv/rhel5-hv-driver-uninstall
@@ -1,15 +1,0 @@
-echo "Get rid of the Hyper-V drivers and modprobe rules"
-
-rm -f  /etc/X11/xorg.conf
-
-rm -rf  /lib/modules/$(uname -r)/extra/hv_*.ko
-rm -rf  /lib/modules/$(uname -r)/extra/hid*.ko
-
-
-echo "Create and install initramfs without Hyper-V drivers"
-
-rm -f "initrd-$(uname -r).img"
-
-mkinitrd "initrd-$(uname -r).img" "$(uname -r)"
-
-mv -f "initrd-$(uname -r).img" /boot/"initrd-$(uname -r).img"

commit 89a6e073beb35159cfbdd23bb1f359a5a6dd4772

RH7: fix recognition of the CRASH MSR for RHEL 7.0 and 7.1
We should test the .misc_features for this bit.

The issue was originally introduced in
https://github.com/LIS/lis-next/commit/8e4be427dfcbe811c27fbde4040c1f90de5b59ea

hv-rhel7.x/hv/vmbus_drv.c
@@ -1115,11 +1115,7 @@ static int vmbus_bus_init(int irq)
 	/*
          * Only register if the crash MSRs are available
          */
-#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,2)) 
 	if (ms_hyperv_ext.misc_features & HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE) {
-#else
-	if (ms_hyperv_ext.features & HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE) {
-#endif
 		register_die_notifier(&hyperv_die_block);
                 atomic_notifier_chain_register(&panic_notifier_list,
                                                &hyperv_panic_block);
@@ -1858,11 +1854,8 @@ static void __exit vmbus_exit(void)
 		tasklet_kill(&hv_cpu->msg_dpc);
 	}
 	vmbus_free_channels();
-#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7,2))
+
         if (ms_hyperv_ext.misc_features & HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE) {
-#else
-        if (ms_hyperv_ext.features & HV_FEATURE_GUEST_CRASH_MSR_AVAILABLE) {
-#endif
 		unregister_die_notifier(&hyperv_die_block);
 		atomic_notifier_chain_unregister(&panic_notifier_list,
 						 &hyperv_panic_block);