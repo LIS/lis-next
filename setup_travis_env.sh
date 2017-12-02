cb34c4aae182c9c1aed1dc4c02cef13b00114287
Chris Valean <cvalean@cloudbasesolutions.com>
2017-12-01 18:03:51 -0800

    Update setup_travis_env.sh

--- setup_travis_env.sh
+++ setup_travis_env.sh
@@ -15,6 +15,15 @@ if [[ "$BUILD" == "7.4.1708" ]]; then
   docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget http://mirror.centos.org/centos/7/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
 elif [[ "$BUILD" == "7."* ]]; then
   docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget http://88.208.217.170/${BUILD}/os/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
+  #
+  # handling updated kernels
+  #
+  if [[ "$KERNEL" == "3.10.0-693.5.2" ]]; then
+    docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget http://88.208.217.170/${BUILD}/updates/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
+  fi
+  if [[ "$KERNEL" == "3.10.0-514.26.2" ]]; then
+    docker exec -t $DOCKER_CONTAINER_ID /bin/bash -xec "wget http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-devel-${KERNEL}.el7.x86_64.rpm"
+  fi
 fi
 
 if [[ "$BUILD" == "6.9" ]]; then
