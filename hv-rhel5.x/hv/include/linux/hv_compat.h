aaf11c9b7aab3236a34dc838e29357d8c995ada3
Chris Valean <cvalean@cloudbasesolutions.com>
2018-03-16 09:32:06 +0200

    Update hv_compat.h

--- hv-rhel5.x/hv/include/linux/hv_compat.h
+++ hv-rhel5.x/hv/include/linux/hv_compat.h
@@ -8,7 +8,7 @@
 #define CN_VSS_IDX	0xA
 #define CN_VSS_VAL	0x1
 
-#define HV_DRV_VERSION  "4.2.4-beta5"
+#define HV_DRV_VERSION  "4.2.4"
 
 #ifndef O_CLOEXEC
 #define O_CLOEXEC       02000000        /* set close_on_exec */
