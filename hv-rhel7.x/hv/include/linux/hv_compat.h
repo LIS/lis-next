d216f5e514b47b0fb8622bc0a4944633a5c0a59d
Vivek Yadav <vyadav@microsoft.com>
2018-06-19 16:28:07 -0700

    Update version

--- hv-rhel7.x/hv/include/linux/hv_compat.h
+++ hv-rhel7.x/hv/include/linux/hv_compat.h
@@ -36,7 +36,7 @@
 #define rdtscll(now)    do { (now) = rdtsc_ordered(); } while (0)
 #endif
 
-#define HV_DRV_VERSION	"master"
+#define HV_DRV_VERSION	"4.2.5"
 #define _HV_DRV_VERSION 0x1A8
 
 #ifdef __KERNEL__
