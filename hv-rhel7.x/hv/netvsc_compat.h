9a19f25d4256c70f6759bdac879ac4108d3cdf11
DivyaJyothiKasam <divya.kasam@wipro.com>
2018-08-30 14:47:44 +0530

    RH7: hv_netvsc: Add per-cpu ethtool stats for netvsc

--- hv-rhel7.x/hv/netvsc_compat.h
+++ hv-rhel7.x/hv/netvsc_compat.h
@@ -21,3 +21,30 @@ static inline void __napi_schedule_irqoff(struct napi_struct *n)
 }
 #endif
 
+
+
+static inline void *compat_kvmalloc_array(size_t n, size_t size, gfp_t flags)
+{
+        void *ptr = NULL;
+#if (RHEL_RELEASE_CODE <= RHEL_RELEASE_VERSION(7,4))
+         ptr = vzalloc(n*size);
+#else
+         ptr = kvmalloc_array(n, size, flags);
+#endif
+        return ptr;
+}
+
+
+#define kvmalloc_array compat_kvmalloc_array
+
+static inline void compat_kvfree(const void *addr)
+{
+#if (RHEL_RELEASE_CODE <= RHEL_RELEASE_VERSION(7,4))
+        vfree(addr);
+#else
+        kvfree(addr);
+#endif
+}
+
+#define kvfree compat_kvfree
+
