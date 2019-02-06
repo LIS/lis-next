b944407d15e58025a6769a1df9ed053c32e31781
Johnson <v-jogeor@microsoft.com>
2019-02-06 00:44:57 -0800

    RH6: hv: vmbus: Offload the handling of channels to two workqueues
    Backported from RH7: SHA: e7fb7877ccf2a90fec7696f4e66e86b4d4d3fcbf
    
    In this backport, modification related to vmbus_add_channel_kobj()
    is skipped since this function is not supported in RH6.X. This was
    added n RH7.X as part of other commit (6e6c3c9d31aed2e132) for
    per-channel sysfs info.

--- hv-rhel6.x/hv/include/linux/hyperv.h
+++ hv-rhel6.x/hv/include/linux/hyperv.h
@@ -917,7 +917,13 @@ struct vmbus_channel {
 	enum hv_numa_policy affinity_policy;
 
 	bool probe_done;
-
+	/*
+	 * We must offload the handling of the primary/sub channels
+	 * from the single-threaded vmbus_connection.work_queue to
+	 * two different workqueue, otherwise we can block
+	 * vmbus_connection.work_queue and hang: see vmbus_process_offer().
+	 */
+	struct work_struct add_channel_work;
 };
 
 static inline bool is_hvsock_channel(const struct vmbus_channel *c)
