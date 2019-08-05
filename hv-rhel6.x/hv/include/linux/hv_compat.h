33682ab679ccd6e2a582960ff91c424e4776936d
Johnson <v-jogeor@microsoft.com>
2019-08-05 09:57:43 -0700

    RH5 RH6 RH7: Version update for LIS 4.3.4 release

--- hv-rhel6.x/hv/include/linux/hv_compat.h
+++ hv-rhel6.x/hv/include/linux/hv_compat.h
@@ -30,8 +30,8 @@
 #define CN_VSS_IDX	0xA
 #define CN_VSS_VAL	0x1
 
-#define HV_DRV_VERSION	"master"
-#define _HV_DRV_VERSION 0x1A8
+#define HV_DRV_VERSION	"4.3.4"
+#define _HV_DRV_VERSION 0x1B2
 
 #ifdef __KERNEL__
 
