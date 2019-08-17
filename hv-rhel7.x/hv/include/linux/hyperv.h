LIS/lis-next

commit e15010d8d63ec20846e15adcc2a29e739e69a5d1

RH7: scsi: storvsc: Select channel based on available percentage of ring buffer to write
Backported from upstream:
https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/commit/?id=2217a47de42f85b69714c2a621af13cfeae35b40

hv-rhel7.x/hv/storvsc_drv.c
@@ -417,6 +417,12 @@ MODULE_PARM_DESC(storvsc_ringbuffer_size, "Ring buffer size (bytes)");
 
 module_param(storvsc_vcpus_per_sub_channel, int, S_IRUGO);
 MODULE_PARM_DESC(storvsc_vcpus_per_sub_channel, "Ratio of VCPUs to subchannels");
+
+static int ring_avail_percent_lowater = 10;
+module_param(ring_avail_percent_lowater, int, S_IRUGO);
+MODULE_PARM_DESC(ring_avail_percent_lowater,
+		"Select a channel if available ring size > this in percent");
+
 /*
  * Timeout in seconds for all devices managed by this driver.
  */
@@ -501,6 +507,13 @@ struct storvsc_device {
 	 * Mask of CPUs bound to subchannels.
 	 */
 	struct cpumask alloced_cpus;
+	/*
+	 * Pre-allocated struct cpumask for each hardware queue.
+	 * struct cpumask is used by selecting out-going channels. It is a
+	 * big structure, default to 1024k bytes when CONFIG_MAXSMP=y.
+	 * Pre-allocate it to avoid allocation on the kernel stack.
+	 */
+	struct cpumask *cpumask_chns;
 	/* Used for vsc/vsp channel reset process */
 	struct storvsc_cmd_request init_request;
 	struct storvsc_cmd_request reset_request;
@@ -1183,6 +1196,13 @@ static int storvsc_channel_init(struct hv_device *device, bool is_fc)
 	if (stor_device->stor_chns == NULL)
 		return -ENOMEM;
 
+	stor_device->cpumask_chns = kcalloc(num_possible_cpus(),
+			sizeof(struct cpumask), GFP_KERNEL);
+	if (stor_device->cpumask_chns == NULL) {
+		kfree(stor_device->stor_chns);
+		return -ENOMEM;
+	}
+
 	stor_device->stor_chns[device->channel->target_cpu] = device->channel;
 	cpumask_set_cpu(device->channel->target_cpu,
 			&stor_device->alloced_cpus);
@@ -1597,6 +1617,7 @@ static int storvsc_dev_remove(struct hv_device *device)
 	vmbus_close(device->channel);
 
 	kfree(stor_device->stor_chns);
+	kfree(stor_device->cpumask_chns);
 	kfree(stor_device);
 	return 0;
 }
@@ -1606,7 +1627,7 @@ static struct vmbus_channel *get_og_chn(struct storvsc_device *stor_device,
 {
 	u16 slot = 0;
 	u16 hash_qnum;
-	struct cpumask alloced_mask;
+	struct cpumask *alloced_mask = &stor_device->cpumask_chns[q_num];
 	int num_channels, tgt_cpu;
 
 	if (stor_device->num_sc == 0)
@@ -1622,10 +1643,10 @@ static struct vmbus_channel *get_og_chn(struct storvsc_device *stor_device,
 	 * III. Mapping is persistent.
 	 */
 
-	cpumask_and(&alloced_mask, &stor_device->alloced_cpus,
+	cpumask_and(alloced_mask, &stor_device->alloced_cpus,
 		    cpumask_of_node(cpu_to_node(q_num)));
 
-	num_channels = cpumask_weight(&alloced_mask);
+	num_channels = cpumask_weight(alloced_mask);
 	if (num_channels == 0)
 		return stor_device->device->channel;
 
@@ -1633,7 +1654,7 @@ static struct vmbus_channel *get_og_chn(struct storvsc_device *stor_device,
 	while (hash_qnum >= num_channels)
 		hash_qnum -= num_channels;
 
-	for_each_cpu(tgt_cpu, &alloced_mask) {
+	for_each_cpu(tgt_cpu, alloced_mask) {
 		if (slot == hash_qnum)
 			break;
 		slot++;
@@ -1649,9 +1670,9 @@ static int storvsc_do_io(struct hv_device *device,
 {
 	struct storvsc_device *stor_device;
 	struct vstor_packet *vstor_packet;
-	struct vmbus_channel *outgoing_channel;
+	struct vmbus_channel *outgoing_channel, *channel;
 	int ret = 0;
-	struct cpumask alloced_mask;
+	struct cpumask *alloced_mask;
 	int tgt_cpu;
 
 	vstor_packet = &request->vstor_packet;
@@ -1669,26 +1690,58 @@ static int storvsc_do_io(struct hv_device *device,
 	 */
 	if (stor_device->stor_chns[q_num] != NULL) {
 		outgoing_channel = stor_device->stor_chns[q_num];
-		if (outgoing_channel->target_cpu == smp_processor_id()) {
+		if (outgoing_channel->target_cpu == q_num) {
 			/*
 			 * Ideally, we want to pick a different channel if
 			 * available on the same NUMA node.
 			 */
-			cpumask_and(&alloced_mask, &stor_device->alloced_cpus,
+			alloced_mask = &stor_device->cpumask_chns[q_num];
+			cpumask_and(alloced_mask, &stor_device->alloced_cpus,
 				    cpumask_of_node(cpu_to_node(q_num)));
-			for_each_cpu_wrap(tgt_cpu, &alloced_mask,
-					outgoing_channel->target_cpu + 1) {
-				if (tgt_cpu != outgoing_channel->target_cpu) {
-					outgoing_channel =
-					stor_device->stor_chns[tgt_cpu];
-					break;
+
+			for_each_cpu_wrap(tgt_cpu, alloced_mask, q_num + 1) {
+				if (tgt_cpu == q_num)
+					continue;
+				channel = stor_device->stor_chns[tgt_cpu];
+				if (hv_get_avail_to_write_percent(
+							&channel->outbound)
+						> ring_avail_percent_lowater) {
+					outgoing_channel = channel;
+					goto found_channel;
+				}
+			}
+
+			/*
+			 * All the other channels on the same NUMA node are
+			 * busy. Try to use the channel on the current CPU
+			 */
+			if (hv_get_avail_to_write_percent(
+						&outgoing_channel->outbound)
+					> ring_avail_percent_lowater)
+				goto found_channel;
+
+			/*
+			 * If we reach here, all the channels on the current
+			 * NUMA node are busy. Try to find a channel in
+			 * other NUMA nodes
+			 */
+			cpumask_andnot(alloced_mask, &stor_device->alloced_cpus,
+					cpumask_of_node(cpu_to_node(q_num)));
+
+			for_each_cpu(tgt_cpu, alloced_mask) {
+				channel = stor_device->stor_chns[tgt_cpu];
+				if (hv_get_avail_to_write_percent(
+							&channel->outbound)
+						> ring_avail_percent_lowater) {
+					outgoing_channel = channel;
+					goto found_channel;
 				}
 			}
 		}
 	} else {
 		outgoing_channel = get_og_chn(stor_device, q_num);
 	}
-
+found_channel:
 	vstor_packet->flags |= REQUEST_COMPLETION_FLAG;
 
 	vstor_packet->vm_srb.length = (sizeof(struct vmscsi_request) -
@@ -2221,8 +2274,9 @@ static int storvsc_probe(struct hv_device *device,
 		max_sub_channels = (num_cpus / storvsc_vcpus_per_sub_channel);
 	}
 
-	scsi_driver.can_queue = (max_outstanding_req_per_channel *
-				 (max_sub_channels + 1));
+	scsi_driver.can_queue = max_outstanding_req_per_channel *
+				(max_sub_channels + 1) *
+				(100 - ring_avail_percent_lowater) / 100;
 
 	host = scsi_host_alloc(&scsi_driver,
 			       sizeof(struct hv_host_device));
@@ -2359,6 +2413,7 @@ err_out2:
 
 err_out1:
 	kfree(stor_device->stor_chns);
+	kfree(stor_device->cpumask_chns);
 	kfree(stor_device);
 
 err_out0:

commit 504173b28d348150fcbfb3a289e8594a06c97e30

RH7.0: Support hv_get_avail_to_write_percent for RH7.0
Fixes: e15010d8d63e ("RH7: scsi: storvsc: Select channel based on available
percentage of ring buffer to write)

hv-rhel7.x/hv/include/linux/hyperv.h
@@ -36,14 +36,16 @@
 #include <linux/device.h>
 #include <linux/mod_devicetable.h>
 #include <linux/interrupt.h>
-#if (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7, 0))
 #include <linux/reciprocal_div.h>
-#endif
 
 #define MAX_PAGE_BUFFER_COUNT				32
 #define MAX_MULTIPAGE_BUFFER_COUNT			32 /* 128K */
 
 #pragma pack(push, 1)
+
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
+extern u32 hv_reciprocal_value;
+#endif
 
 /* Single-page buffer */
 struct hv_page_buffer {
@@ -183,16 +185,25 @@ static inline u32 hv_get_bytes_to_write(const struct hv_ring_buffer_info *rbi)
 	return write;
 }
 
-#if (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7, 0))
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
+static inline void hv_set_reciprocal_value(u32 ring_buffer_size)
+{
+	hv_reciprocal_value = reciprocal_value(ring_buffer_size);
+}
+#endif
+
 static inline u32 hv_get_avail_to_write_percent(const struct hv_ring_buffer_info *rbi)
 {
 	u32 avail_write = hv_get_bytes_to_write(rbi);
-
+#if (RHEL_RELEASE_CODE > RHEL_RELEASE_VERSION(7, 0))
 	return reciprocal_divide(
 		(avail_write << 3) + (avail_write << 1),
 		rbi->ring_size_div10_reciprocal);
-}
+#else
+	BUG_ON(hv_reciprocal_value == 0);
+	return reciprocal_divide(avail_write * 100, hv_reciprocal_value);
 #endif
+}
 
 	/*
  * VMBUS version is 32 bit entity broken up into

hv-rhel7.x/hv/netvsc.c
@@ -35,12 +35,6 @@
 #include "hyperv_net.h"
 #include "netvsc_trace.h"
 
-#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7,0))
-#include <linux/reciprocal_div.h>
-#else
-#include <linux/hyperv.h>
-#endif
-
 /*
  * Switch the data path from the synthetic interface to the VF
  * interface.
@@ -676,18 +670,6 @@ void netvsc_device_remove(struct hv_device *device)
 #define RING_AVAIL_PERCENT_HIWATER 20
 #define RING_AVAIL_PERCENT_LOWATER 10
 
-#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-/*
- * Get the percentage of available bytes to write in the ring.
- * The return value is in range from 0 to 100.
- */
-static u32 hv_ringbuf_avail_percent(const struct hv_ring_buffer_info *ring_info)
-{
-	u32 avail_write = hv_get_bytes_to_write(ring_info);
-	return reciprocal_divide(avail_write * 100, netvsc_ring_reciprocal);
-}
-#endif
-
 static inline void netvsc_free_send_slot(struct netvsc_device *net_device,
 					 u32 index)
 {
@@ -741,16 +723,10 @@ static void netvsc_send_tx_complete(struct net_device *ndev,
 		struct netdev_queue *txq = netdev_get_tx_queue(ndev, q_idx);
 
 		if (netif_tx_queue_stopped(txq) && !net_device->tx_disable &&
-#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-			 (hv_ringbuf_avail_percent(&channel->outbound) > RING_AVAIL_PERCENT_HIWATER ||
-			 queue_sends < 1))
-		{
-#else
 			(hv_get_avail_to_write_percent(&channel->outbound) >
 			 RING_AVAIL_PERCENT_HIWATER ||
 			 queue_sends < 1))
 		{
-#endif
 			netif_tx_wake_queue(txq);
 			ndev_ctx->eth_stats.wake_queue++;
 		}
@@ -865,11 +841,7 @@ static inline int netvsc_send_pkt(
 	struct netdev_queue *txq = netdev_get_tx_queue(ndev, packet->q_idx);
 	u64 req_id;
 	int ret;
-#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-	u32 ring_avail = hv_ringbuf_avail_percent(&out_channel->outbound);
-#else
 	u32 ring_avail = hv_get_avail_to_write_percent(&out_channel->outbound);
-#endif
 
 	nvmsg.hdr.msg_type = NVSP_MSG1_TYPE_SEND_RNDIS_PKT;
 	if (skb)

hv-rhel7.x/hv/netvsc_drv.c
@@ -47,10 +47,6 @@
 
 #include "hyperv_net.h"
 
-#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-#include <linux/reciprocal_div.h>
-#endif
-
 #define RING_SIZE_MIN	64
 #define RETRY_US_LO	5000
 #define RETRY_US_HI	10000
@@ -65,7 +61,7 @@ MODULE_PARM_DESC(ring_size, "Ring buffer size (# of pages)");
 unsigned int netvsc_ring_bytes;
 
 #if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-u32 netvsc_ring_reciprocal;
+u32 hv_reciprocal_value;
 #endif
 
 static const u32 default_msg = NETIF_MSG_DRV | NETIF_MSG_PROBE |
@@ -2549,7 +2545,7 @@ static int __init netvsc_drv_init(void)
 	}
 	netvsc_ring_bytes = ring_size * PAGE_SIZE;
 #if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
-	netvsc_ring_reciprocal = reciprocal_value(netvsc_ring_bytes);
+	hv_set_reciprocal_value(netvsc_ring_bytes);
 #endif
 
 	ret = vmbus_driver_register(&netvsc_drv);

hv-rhel7.x/hv/storvsc_drv.c
@@ -425,6 +425,9 @@ module_param(ring_avail_percent_lowater, int, S_IRUGO);
 MODULE_PARM_DESC(ring_avail_percent_lowater,
 		"Select a channel if available ring size > this in percent");
 
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
+u32 hv_reciprocal_value;
+#endif
 /*
  * Timeout in seconds for all devices managed by this driver.
  */
@@ -2481,6 +2484,9 @@ static int __init storvsc_drv_init(void)
 {
 	int ret;
 
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7, 0))
+	hv_set_reciprocal_value(storvsc_ringbuffer_size);
+#endif
 	/*
 	 * Divide the ring buffer data size (which is 1 page less
 	 * than the ring buffer size since that page is reserved for