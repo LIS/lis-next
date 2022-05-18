874c4ede8c1e2f72a75e1e05761471128fc863b9
Johnson <johnson.george@microsoft.com>
2022-05-17 21:12:02 -0700

    Revert "RH6: scsi: storvsc: Fix scsi_cmd error assignments in storvsc_handle_error"
    This reverts commit 5031856ae3ab0b9e29a64589401c3c2dfcc62103.

--- hv-rhel6.x/hv/storvsc_drv.c
+++ hv-rhel6.x/hv/storvsc_drv.c
@@ -1261,11 +1261,10 @@ static void storvsc_handle_error(struct vmscsi_request *vm_srb,
 		case TEST_UNIT_READY:
 			break;
 		default:
-			set_host_byte(scmnd, DID_ERROR);
+			set_host_byte(scmnd, DID_TARGET_FAILURE);
 		}
 		break;
 	case SRB_STATUS_INVALID_LUN:
-		set_host_byte(scmnd, DID_NO_CONNECT);
 		do_work = true;
 		process_err_fn = storvsc_remove_lun;
 		break;
